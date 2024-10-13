529e601efe5b5b6b6402fdfee351a46be5ee7a32
Tim Heinemann <tim.heinemann@clintworld.de>
2024-10-13 23:19:40 +0200

    new stuff

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
+  function loadReadme() {
+    const readmeUrl = "https://github.com/t1mdotcom/t1mdotcom/blob/main/README.md";
+
+    fetch(readmeUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Coud not load file " + response.statusText);
+        }
+        return response.text();
+      })
+      .then((markdown) => {
+        // Markdown in HTML konvertieren
+        const dirtyHtml = marked(markdown);
+        // HTML-Inhalt säubern
+        const cleanHtml = DOMPurify.sanitize(dirtyHtml);
+        // Den gesäuberten HTML-Inhalt in die Seite einfügen
+        document.getElementById("readme-content").innerHTML = cleanHtml;
+      })
+      .catch((error) => {
+        console.error("Fehler beim Laden des README:", error);
+        document.getElementById("readme-content").innerHTML = "<p>Inhalt konnte nicht geladen werden.</p>";
+      });
+  }
+
+  // Überprüfen, ob der "Über mich"-Bereich angezeigt wird
+  function checkAndLoadReadme() {
+    const aboutSection = document.getElementById("ueber-mich");
+    if (aboutSection) {
+      loadReadme();
+    }
+  }
+
+  // Falls du dynamische Navigation verwendest, rufe die Funktion beim Laden des "Über mich"-Bereichs auf
+  checkAndLoadReadme();
   // Alle Navigationslinks abrufen
   const homeLink = document.querySelectorAll(".header-left a");
   const navLinks = document.querySelectorAll(".header-right a");
@@ -22,24 +56,12 @@ document.addEventListener("DOMContentLoaded", function () {
     let content = "";
 
     if (section === "contact") {
-      content = `
-                <h2>Kontakt</h2>
-                <p>Hier findest du meine Kontaktdaten.</p>
-            `;
+      content = contactContent;
     } else if (section === "projects") {
-      content = `
-                <h2>Projekte</h2>
-                <div id="projects">
-                    <!-- Projekte werden hier geladen -->
-                </div>
-            `;
-      // Endlos-Scrolling für Projekte initiieren
+      content = projectContent;
       implementInfiniteScroll();
     } else if (section === "about-me") {
-      content = `
-                <h2>Über mich</h2>
-                <p>Informationen über mich.</p>
-            `;
+      content = aboutMeContent;
     } else {
       content = defaultContent;
     }
@@ -85,4 +107,56 @@ const defaultContent = `
 <ul>
   <li><a href="https://keyserver.ubuntu.com/pks/lookup?search=tim%40heinemann.foo&fingerprint=on&op=index">Public GPG-Key</a></li>
 </ul>
+`;
+
+const contactContent = `
+<section id="contact">
+    <h2>Contact</h2>
+    <p>You can reach me via e-mail:</p>
+    <ul class="contact-info">
+        <li><a href="mailto:tim@heinemann.foo">tim@heinemann.foo</a></li>
+    </ul>
+    <p>
+      You can check my public key for my e-mail here:
+      <a class="link" href="https://keyserver.ubuntu.com/pks/lookup?search=tim%40heinemann.foo&fingerprint=on&op=index">Public GPG-Key</a>
+    </p>
+
+    <br />
+    <br />
+
+    <h3>Follow me on:</h3>
+    <ul class="social-links">
+        <li>
+            <a href="https://www.xing.com/profile/Tim_Heinemann15" target="_blank" rel="noopener">
+                <i class="nf nf-fa-square_xing"></i> Xing
+            </a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/tim-heinemann-524764190/" target="_blank" rel="noopener">
+                <i class="nf nf-md-linkedin"></i> LinkedIn
+            </a>
+        </li>
+        <li>
+            <a href="https://github.com/t1mdotcom" target="_blank" rel="noopener">
+                <i class="nf nf-md-github"></i> GitHub
+            </a>
+        </li>
+    </ul>
+</section>
+`;
+
+const aboutMeContent = `
+<section id="about-me">
+  <h2>About Me</h2>
+  <div id="readme-content" class="markdown-body">
+
+  </div>
+</section>
+`;
+
+const projectContent = `
+<h2>Projekte</h2>
+<div id="projects">
+    <!-- Projekte werden hier geladen -->
+</div>
 `;

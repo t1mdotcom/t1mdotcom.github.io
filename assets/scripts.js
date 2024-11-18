7d4710682dbde91cf1ecb9330ddd098b30a3ca80
Tim Heinemann <tim.heinemann@clintworld.de>
2024-11-18 10:49:27 +0100

    seperate html files

--- assets/scripts.js
+++ assets/scripts.js
@@ -51,19 +51,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  function loadContent(section) {
+  async function loadContent(section) {
     // Inhalt je nach Abschnitt definieren
     let content = "";
 
     if (section === "contact") {
-      content = contactContent;
+      content = await fetch('./pages/contact.html');
     } else if (section === "projects") {
-      content = projectContent;
+      content = await fetch('./pages/projects.html');
       implementInfiniteScroll();
     } else if (section === "about-me") {
-      content = aboutMeContent;
+      content = await fetch('./pages/about_me.html');
     } else {
-      content = defaultContent;
+      content = await fetch('./pages/home.html');
     }
 
     mainContent.innerHTML = content;
@@ -102,61 +102,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-
-const defaultContent = `
-<ul>
-  <li><a href="https://keyserver.ubuntu.com/pks/lookup?search=tim%40heinemann.foo&fingerprint=on&op=index">Public GPG-Key</a></li>
-</ul>
-`;
-
-const contactContent = `
-<section id="contact">
-    <h2>Contact</h2>
-    <p>You can reach me via e-mail:</p>
-    <ul class="contact-info">
-        <li><a href="mailto:tim@heinemann.foo">tim@heinemann.foo</a></li>
-    </ul>
-    <p>
-      You can check my public key for my e-mail here:
-      <a class="link" href="https://keyserver.ubuntu.com/pks/lookup?search=tim%40heinemann.foo&fingerprint=on&op=index">Public GPG-Key</a>
-    </p>
-
-    <br />
-    <br />
-
-    <h3>Follow me on:</h3>
-    <ul class="social-links">
-        <li>
-            <a href="https://www.xing.com/profile/Tim_Heinemann15" target="_blank" rel="noopener">
-                <i class="nf nf-fa-square_xing"></i> Xing
-            </a>
-        </li>
-        <li>
-            <a href="https://www.linkedin.com/in/tim-heinemann-524764190/" target="_blank" rel="noopener">
-                <i class="nf nf-md-linkedin"></i> LinkedIn
-            </a>
-        </li>
-        <li>
-            <a href="https://github.com/t1mdotcom" target="_blank" rel="noopener">
-                <i class="nf nf-md-github"></i> GitHub
-            </a>
-        </li>
-    </ul>
-</section>
-`;
-
-const aboutMeContent = `
-<section id="about-me">
-  <h2>About Me</h2>
-  <div id="readme-content" class="markdown-body">
-
-  </div>
-</section>
-`;
-
-const projectContent = `
-<h2>Projekte</h2>
-<div id="projects">
-    <!-- Projekte werden hier geladen -->
-</div>
-`;

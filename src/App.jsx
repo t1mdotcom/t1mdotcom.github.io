1bd46345f76e8bf2e658c56b8eb9b9603f1b4a66
Tim Heinemann <tim.heinemann@clintworld.de>
2025-07-07 10:27:31 +0200

    remove: production build files from version control

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
+import {PrimeReactProvider} from 'primereact/api';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
-  return (
-    <Router>
-      <div className="wrapper">
-        <Header />
-        <main id="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about-me" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </div>
-      <Footer />
-    </Router>
-  )
+	return (
+			<PrimeReactProvider>
+				<Router>
+					<div className="wrapper">
+						<Header/>
+						<main id="main-content" className="min-h-screen bg-gray-900 text-gray-300">
+							<Routes>
+								<Route path="/" element={<Home/>}/>
+								<Route path="/home" element={<Home/>}/>
+								<Route path="/about-me" element={<About/>}/>
+								<Route path="/projects" element={<Projects/>}/>
+								<Route path="/contact" element={<Contact/>}/>
+							</Routes>
+						</main>
+					</div>
+					<Footer/>
+				</Router>
+			</PrimeReactProvider>
+	);
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import {Card} from 'primereact/card';
+import React from 'react';
 
 function Home() {
-  return (
-    <div>
-      <ul>
-        <li>
-          <a href="https://keyserver.ubuntu.com/pks/lookup?search=tim%40heinemann.foo&fingerprint=on&op=index">
-            Public GPG-Key
-          </a>
-        </li>
-      </ul>
-    </div>
-  )
+	return (
+			<Card title="E-Mail Signature">
+				<div>
+					<ul>
+						<li>
+							<a href="https://keyserver.ubuntu.com/pks/lookup?search=tim%40heinemann.foo&fingerprint=on&op=index">
+								Public GPG-Key
+							</a>
+						</li>
+					</ul>
+				</div>
+			</Card>
+	);
 }
 
-export default Home
+export default Home;

b12e0e1376810c270a2c42e35b0035ffb9bd6aa0
Tim Heinemann <tim.heinemann@clintworld.de>
2025-07-07 09:06:30 +0200

    add: plugin to generate custom CNAME during build for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-cname',
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: 'CNAME',
+          source: 'www.heinemann.foo'
+        });
+      }
+    }
+  ],
   base: '/',
   build: {
     outDir: 'dist',
